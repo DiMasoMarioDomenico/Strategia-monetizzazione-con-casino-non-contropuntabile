aaacd9d900be8465e0a0ee516dca4bf70be7ac17
DiMasoMarioDomenico <mario.domenico@hotmail.it>
2024-09-13 15:50:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,8 +17,9 @@ function calcola() {
     var rischioBasso = volumeTotale * 0.2;
     var guadagnoBasso = volumeContribuzione - rischioBasso;
 
-    document.getElementById("costoStimatoSlot").innerHTML = "Costo stimato SLOT per completamento rollover: €" + costoStimato.toFixed(2);
     document.getElementById("volumeTotale").innerHTML = "Volume totale da generare: €" + volumeTotale.toFixed(2);
+    document.getElementById("volumeContribuzione").innerHTML = "Volume da generare con contribuzione giochi: €" + volumeContribuzione.toFixed(2);
+    document.getElementById("costoStimatoSlot").innerHTML = "Costo stimato SLOT per completamento rollover: €" + costoStimato.toFixed(2);
     document.getElementById("rischioAlto").innerHTML = "Livello Rischio Alto: €" + rischioAlto.toFixed(2);
     document.getElementById("guadagnoAlto").innerHTML = "Guadagno atteso: €" + guadagnoAlto.toFixed(2);
     document.getElementById("rischioMedio").innerHTML = "Livello Rischio Medio: €" + rischioMedio.toFixed(2);
